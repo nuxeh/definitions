nuxeh/morph

rawdisk.write: Add functions to create filesystems on partitions
These functions read the partition specification data structure and
creates the required filesystems on the specified partitions in a disc
image, or on a device.

Change-Id: I84dc3e46f863a2f19159c5345fc8194e43bda874

morphlib/writeexts.py
@@ -696,6 +696,8 @@ class WriteExtension(cliapp.Application):
                          partition_data_raw, sector_size)
 
         self.create_partition_table(location, partition_data)
+        partitions = partition_data['partitions']
+        self.create_partition_filesystems(location, partitions, sector_size)
 
     def load_partition_data(self, part_file):
         ''' Load partition data from a yaml specification '''
@@ -820,3 +822,36 @@ class WriteExtension(cliapp.Application):
         cmd += ("w\n"
                "q\n")
         cliapp.runcmd(['fdisk', location], feed_stdin=cmd)
+
+    def create_partition_filesystems(self, location, partitions, sector_size):
+        ''' Read partition data and create all required filesystems '''
+
+        self.status(msg="Creating filesystems")
+
+        for partition in partitions:
+            filesystem = partition['format']
+            if filesystem not in ('none', 'None', None):
+                with self.create_loopback(location,
+                                          partition['start'] *
+                                          sector_size,
+                                          partition['size']) as device:
+                    self.create_filesystem(device, filesystem)
+
+    def create_filesystem(self, block_device, fstype):
+        ''' Create filesystems of various types on a device node '''
+
+        recognised_filesystem_formats = ('btrfs', 'ext4', 'vfat')
+
+        if fstype == 'btrfs':
+            self.format_btrfs(block_device)
+        elif fstype in recognised_filesystem_formats:
+            try:
+                self.status(msg='Creating %s filesystem' % fstype)
+                cliapp.runcmd(['mkfs.' + fstype, block_device])
+            except BaseException:
+                raise cliapp.AppException(
+                        'Error creating %s filesystem on %s'
+                        % (fstype, block_device))
+        else:
+            raise cliapp.AppException('Unrecognised filesystem'
+                                      ' format: %s' % fstype)

nuxeh/definitions

extensions/writeexts.py
@@ -826,6 +826,8 @@ class WriteExtension(Extension):
                          partition_data_raw, sector_size)
 
         self.create_partition_table(location, partition_data)
+        partitions = partition_data['partitions']
+        self.create_partition_filesystems(location, partitions, sector_size)
 
     def load_partition_data(self, part_file):
         ''' Load partition data from a yaml specification '''
@@ -1063,3 +1065,37 @@ class WriteExtension(Extension):
                              stderr=subprocess.PIPE,
                              stdout=subprocess.PIPE)
         p.communicate(cmd)
+
+    def create_partition_filesystems(self, location, partitions, sector_size):
+        ''' Read partition data and create all required filesystems '''
+
+        self.status(msg="Creating filesystems...")
+
+        for partition in partitions:
+            filesystem = partition['format']
+            if filesystem not in ('none', 'None', None):
+                with self.create_loopback(location,
+                                          partition['start'] *
+                                          sector_size,
+                                          partition['size']) as device:
+                    self.status(msg='Creating filesystem on partition %d' %
+                                     partition['number'])
+                    self.create_filesystem(device, filesystem)
+
+    def create_filesystem(self, block_device, fstype):
+        ''' Create filesystems of various types on a device node '''
+
+        recognised_filesystem_formats = ('btrfs', 'ext4', 'vfat')
+
+        if fstype == 'btrfs':
+            self.format_btrfs(block_device)
+        elif fstype in recognised_filesystem_formats:
+            try:
+                self.status(msg='Creating %s filesystem' % fstype)
+                subprocess.check_call(['mkfs.' + fstype, block_device])
+            except BaseException:
+                raise ExtensionError('Error creating %s filesystem on %s' %
+                                     (fstype, block_device))
+        else:
+            raise ExtensionError('Unrecognised filesystem'
+                                 ' format: %s' % fstype)