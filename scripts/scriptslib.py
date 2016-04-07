aa7ad85c099193d62d358c44c3d00e20b8aca618
Edward Cragg <edward.cragg@codethink.co.uk>
2016-04-07 13:42:39 +0100

    Add BaserockMeta class
    Change-Id: I08a32fc11c5d6578916b61cd4264863b0db7b57d

--- scripts/scriptslib.py
+++ scripts/scriptslib.py
@@ -43,3 +43,73 @@ def definitions_root():
 def load_yaml_file(yaml_file):
     with open(yaml_file, 'r') as f:
         return yaml.safe_load(f)
+
+
+class BaserockMeta(object):
+    '''An object representing Baserock metadata contained in a Baserock
+       system image, for available metadata formats'''
+
+    def __init__(self):
+        self.metas = []
+
+    def get_metas(self):
+        return self.metas
+
+    def import_meta(self, meta_text):
+        importers = (self.import_meta_ybd,
+                     self.import_meta_morph)
+
+        for i in importers:
+            try:
+                i(meta_text)
+                return
+            except BaseException:
+                pass
+
+        # Shouldn't get here
+        raise Exception('Metadata format not recognised.')
+
+    def import_meta_morph(self, meta_text):
+        self._add_meta(yaml.load(meta_text))
+
+    def import_meta_ybd(self, meta_text):
+        source = yaml.load(meta_text)
+
+        if 'repo' not in source:
+            return # Probably a stratum (no morph equivalent meta)
+
+        repo = parse_repo_alias(source['repo'])
+        source_name = '-'.join(
+                      source['products'][0]['artifact'].split('-')[:-1])
+
+        for meta in source['products']:
+
+            self._add_meta({
+                'kind':          'chunk',
+                'source_name':   source_name,
+                'artifact_name': meta['artifact'],
+                'repo':          repo,
+                'sha1':          source['ref'],
+                'repo-alias':    source['repo'],
+                'contents':      meta['files']
+                })
+
+    def _add_meta(self, meta_dict):
+        '''Validate and add a meta'''
+
+        required_fields = ('repo', 'sha1', 'contents')
+        for f in required_fields:
+            if meta_dict[f] is None:
+                raise Exception('Metadata format not recognised, '
+                                'no value for \'%s\'' % f)
+        self.metas.append(meta_dict)
+
+
+def meta_load_from_dir(meta_dir_path):
+    '''Return a dict representing a Baserock meta string'''
+    files = [f for f in os.listdir(meta_dir_path)
+            if os.isfile(join(meta_dir_path, f))]
+
+    meta = BasrockMeta()
+    for f in files:
+        meta.import_meta(open(os.join(meta_dir_path, f)).read())
